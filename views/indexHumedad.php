72e667a64cc1f39da504a482f4aa37ccf4130933
AndresFWilT <andresfwilchest@gmail.com>
2021-03-05 13:33:56 -0500

    Commit 4

--- views/indexHumedad.php
+++ views/indexHumedad.php
@@ -3,7 +3,6 @@ $conexionBD = null;
 //Conexion con BD
 try {
     $conexionBD = new PDO('mysql:host=localhost;dbname=estmetereologica', 'root', '');
-    echo "Conexion establecida con la base de datos </br>";
 } catch (PDOException $e) {
     echo "Conexion fallida </br>";
 }
@@ -11,13 +10,24 @@ $intervalo = 0;
 $dia = '05';
 $mes = '03';
 static $impresion = '';
+static $max=0;static $min=0;static $act=0;
+static $fmax='';static $fmin='';static $fact='';
 
 $ano = '2021';
+//Query para sacar datos del dia de la base de datos
 $datos = $conexionBD->query("SELECT UNIX_TIMESTAMP(fecha), humedad FROM datosh WHERE year(fecha) = '$ano' AND month(fecha) = '$mes' AND day(fecha) = '$dia'");
-
+//Query para sacar el dato mas alto
+$resultado = $conexionBD->query("SELECT * FROM datosh WHERE humedad = (SELECT MAX(humedad) from datosh)");
+if($row = $resultado->fetch()){$max = trim($row[2]);$fmax = trim($row[1]);}
+//Query para sacar el dato mas bajo
+$resultado = $conexionBD->query("SELECT * FROM datosh WHERE humedad = (SELECT MIN(humedad) from datosh)");
+if($row = $resultado->fetch()){$min = trim($row[2]);$fmin = trim($row[1]);}
+//Query para sacar el dato actual registrado
+$resultado =    $conexionBD->query("SELECT * FROM datosh WHERE id = (SELECT MAX(id) from datosh)");
+if($row = $resultado->fetch()){$act = trim($row[2]);$fact = trim($row[1]);}
 while ($fila = $datos->fetch()) {
     $impresion = $impresion . "[";
-    $impresion = $impresion . $fila[0];
+    $impresion = $impresion . $fila[0]*1000;
     $impresion = $impresion . ",";
     $impresion = $impresion . $fila[1];
     $impresion = $impresion . "],";
@@ -25,7 +35,6 @@ while ($fila = $datos->fetch()) {
         $fila = $datos->fetch();
     }
 }
-echo $impresion;
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -53,7 +62,22 @@ echo $impresion;
             <div class="card-big">
                 <div class="card">
                     <p class="card-title">HUMEDAD</p>
-                    <p style="text-align: left;"><span class="reading"><span id="hum">Humedad actual: </span>%</span></p>
+                    <p style="text-align: left;"><span class="reading"><span id="temp">Humedad actual: 
+                    <?php echo $act; ?> % <?php for($i=0;$i<45;$i++){echo "&nbsp";}?>
+                    Humedad mas alta registrada:
+                    <?php echo $max; ?> % <?php for($i=0;$i<30;$i++){echo "&nbsp";}?>
+                    Humedad mas baja registrada:
+                    <?php echo $min; ?></span>%
+                    </span>
+                    </p>
+                    <p style="text-align: left;"><span class="reading"><span id="temp"> 
+                    <?php echo $fact; ?>  <?php for($i=0;$i<65;$i++){echo "&nbsp";}?>
+                    
+                    <?php echo $fmax; ?>  <?php for($i=0;$i<60;$i++){echo "&nbsp";}?>
+                    
+                    <?php echo $fmin; ?></span>  
+                    </span>
+                    </p>
                     <div id="chart-humedad"></div>
                 </div>
             </div>
@@ -61,64 +85,65 @@ echo $impresion;
     </div>
 
     <script>
-        graficar();
-        function graficar() {
-            Highcharts.chart('chart-humedad', {
+        //Grafico humedad
+        Highcharts.chart('chart-humedad', {
 
+            title: {
+                text: 'Grafico humedad'
+            },
+
+            subtitle: {
+                text: null
+            },
+
+            yAxis: {
                 title: {
-                    text: 'Grafico humedad'
-                },
-
-                subtitle: {
-                    text: null
-                },
-
-                yAxis: {
-                    title: {
-                        text: 'Humedad (%)'
-                    }
-                },
-
-                xAxis: {
-
-                    accessibility: {
-                        rangeDescription: 'Tiempo'
-                    }
-                },
-
-                plotOptions: {
-                    series: {
-                        label: {
-                            connectorAllowed: false
-                        },
-                        pointStart: 2010
-                    }
-                },
-
-                series: [{
-                    name: 'humedad %',
-                    data: [<?php print_r($impresion); ?>]
-                }, ],
-
-                responsive: {
-                    rules: [{
-                        condition: {
-                            maxWidth: 500
-                        },
-                        chartOptions: {
-                            legend: {
-                                layout: 'horizontal',
-                                align: 'center',
-                                verticalAlign: 'bottom'
-                            }
-                        }
-                    }]
+                    text: 'Humedad (%)'
                 }
+            },
 
-            });
+            xAxis: {
+                type: 'datetime',
+                accessibility: {
+                    rangeDescription: 'Tiempo'
+                }
+            },
+
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false
+                    },
+                    pointStart: 2010
+                }
+            },
+
+            series: [{
+                name: 'humedad %',
+                data: [ <?php print_r($impresion); ?> ]
+            }, ],
+
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        legend: {
+                            layout: 'horizontal',
+                            align: 'center',
+                            verticalAlign: 'bottom'
+                        }
+                    }
+                }]
+            }
+
+        });
+
+        function actualizar() {
+            location.reload(true);
         }
-
-        setInterval(graficar, 5000);
+        setInterval(actualizar, 5000);
     </script>
 </body>
 
--- views/indexTemperatura.php
+++ views/indexTemperatura.php
@@ -2,7 +2,6 @@
 //Conexion con BD
 try {
     $conexionBD = new PDO('mysql:host=localhost;dbname=estmetereologica', 'root', '');
-    echo "Conexion establecida con la base de datos </br>";
 } catch (PDOException $e) {
     echo "Conexion fallida </br>";
 }
@@ -10,13 +9,38 @@ $intervalo = 0;
 $dia = '05';
 $mes = '03';
 static $impresion = '';
+static $max = 0;
+static $min = 0;
+static $act = 0;
+static $fmax = '';
+static $fmin = '';
+static $fact = '';
 
 $ano = '2021';
+//Query para sacar datos del dia de la base de datos
 $datos = $conexionBD->query("SELECT UNIX_TIMESTAMP(fecha), temperatura FROM datost WHERE year(fecha) = '$ano' AND month(fecha) = '$mes' AND day(fecha) = '$dia'");
+//Query para sacar el dato mas alto
+$resultado = $conexionBD->query("SELECT * FROM datost WHERE temperatura = (SELECT MAX(temperatura) from datost)");
+if ($row = $resultado->fetch()) {
+    $max = trim($row[2]);
+    $fmax = trim($row[1]);
+}
+//Query para sacar el dato mas bajo
+$resultado = $conexionBD->query("SELECT * FROM datost WHERE temperatura = (SELECT MIN(temperatura) from datost)");
+if ($row = $resultado->fetch()) {
+    $min = trim($row[2]);
+    $fmin = trim($row[1]);
+}
+//Query para sacar el dato actual registrado
+$resultado =    $conexionBD->query("SELECT * FROM datost WHERE id = (SELECT MAX(id) from datost)");
+if ($row = $resultado->fetch()) {
+    $act = trim($row[2]);
+    $fact = trim($row[1]);
+}
 
 while ($fila = $datos->fetch()) {
     $impresion = $impresion . "[";
-    $impresion = $impresion . $fila[0] * 1000;
+    $impresion = $impresion . $fila[0]*1000;
     $impresion = $impresion . ",";
     $impresion = $impresion . $fila[1];
     $impresion = $impresion . "],";
@@ -50,71 +74,94 @@ while ($fila = $datos->fetch()) {
             <div class="card-big">
                 <div class="card">
                     <p class="card-title">TEMPERATURA</p>
-                    <p style="text-align: left;"><span class="reading"><span id="temp">Temperatura actual: </span> &deg;C</span></p>
+                    <p style="text-align: left;"><span class="reading"><span id="temp"> Temperatura actual:
+                                <?php echo $act; ?> &deg;C <?php for ($i = 0; $i < 40; $i++) {
+                                                                echo "&nbsp";
+                                                            } ?>
+                                Temperatura mas alta registrada:
+                                <?php echo $max; ?> &deg;C <?php for ($i = 0; $i < 15; $i++) {
+                                                                echo "&nbsp";
+                                                            } ?>
+                                Temperatura mas baja registrada:
+                                <?php echo $min; ?></span> &deg;C
+                        </span>
+                    </p>
+                    <p style="text-align: left;"><span class="reading"><span id="temp">
+                                <?php echo $fact; ?> <?php for ($i = 0; $i < 75; $i++) {
+                                                            echo "&nbsp";
+                                                        } ?>
+
+                                <?php echo $fmax; ?> <?php for ($i = 0; $i < 50; $i++) {
+                                                            echo "&nbsp";
+                                                        } ?>
+
+                                <?php echo $fmin; ?></span>
+                        </span>
+                    </p>
                     <div id="chart-temperatura" class="container"></div>
                 </div>
             </div>
         </div>
     </div>
     <script>
-        graficar();
-        //Parte sensor de Temperatura
-        function graficar() {
-            Highcharts.chart('chart-temperatura', {
+        //Grafico temperatura
+        Highcharts.chart('chart-temperatura', {
+            title: {
+                text: 'Grafico temperatura'
+            },
 
+            subtitle: {
+                text: null
+            },
+
+            yAxis: {
                 title: {
-                    text: 'Grafico temperatura'
-                },
-
-                subtitle: {
-                    text: null
-                },
-
-                yAxis: {
-                    title: {
-                        text: 'Temperatura (C)'
-                    }
-                },
-
-                xAxis: {
-
-                    accessibility: {
-                        rangeDescription: 'Tiempo'
-                    }
-                },
-
-                plotOptions: {
-                    series: {
-                        label: {
-                            connectorAllowed: false
-                        },
-                        pointStart: 2010
-                    }
-                },
-
-                series: [{
-                    name: 'temperatura (C)',
-                    data: [<?php print_r($impresion); ?>]
-                }, ],
-
-                responsive: {
-                    rules: [{
-                        condition: {
-                            maxWidth: 500
-                        },
-                        chartOptions: {
-                            legend: {
-                                layout: 'horizontal',
-                                align: 'center',
-                                verticalAlign: 'bottom'
-                            }
-                        }
-                    }]
+                    text: 'Temperatura (C)'
                 }
+            },
 
-            });
+            xAxis: {
+                type: 'datetime',
+                accessibility: {
+                    rangeDescription: 'Tiempo'
+                }
+            },
+
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false
+                    },
+                    pointStart: 2010
+                }
+            },
+
+            series: [{
+                name: 'Temperatura (C)',
+                data: [<?php print_r($impresion); ?>]
+            }, ],
+
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        legend: {
+                            layout: 'horizontal',
+                            align: 'center',
+                            verticalAlign: 'bottom'
+                        }
+                    }
+                }]
+            }
+
+        });
+
+        function actualizar() {
+            location.reload(true);
         }
-        setInterval(graficar, 5000);
+        setInterval(actualizar, 5000);
     </script>
 </body>
 
